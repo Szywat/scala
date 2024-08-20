d94ec0246ab09fbc18d4b318d31501086def4d53
Szywat <szywat04@gmail.com>
2024-08-20 22:30:43 +0200

    obramowanie tekstu

--- lab01/src/main/scala/Main.scala
+++ lab01/src/main/scala/Main.scala
@@ -1,5 +1,21 @@
+def obramuj(napis: String): String = {
+
+  val linie = napis.split('\n') // split rozdziela nam słowa jeśli podany znak istnieje
+  val najdluzsza = linie.maxBy(s => s.length) /// maxBy szuka nam najdłuższego wyrazu bazując na długosci oddzielonych wyrazów
+  val szerokosc = najdluzsza.length() // długość najdłuższego wyrazu zamieniamy na liczbę
+  val ramka = "*" + ("*" * szerokosc) + "*" // ramka to początek, środek (szerokość to ilość gwiazdek w środku), zakończenie
+
+  ramka + "\n" + linie.map(l => "*" + l + (" " * (szerokosc - l.length())) + "*").mkString("\n") + "\n" + ramka
+  // ^ co funkcja ma zwracać
+    // ramka, pominięcie do następnej linijki,
+    // każdy wyraz osobno ze spacjami równej szerokości odjąć długość tych wyrazów,
+    // oddzielenie każdego wyrazu przejściem do następnej linijki
+    // przejście do następnej linijki, zakończenie ramką
+}
+
 @main
-def mainProg: Unit = {
-  println("Hello World!")
+def mainProg(napis: String): Unit = {
+  println(obramuj(napis))
+
 }
 
